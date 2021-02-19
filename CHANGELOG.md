7978c6d1f31a50c06c19c042023642bc85856dae
Adam Hooper <adam@adamhooper.com>
2021-02-19 16:13:45 -0500

    Wait a bit longer before heartbeat-disconnecting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+v0.0.3 - 2021-02-21
+~~~~~~~~~~~~~~~~~~~
+
+* Fix heartbeat-monitoring logic to avoid disconnect when beats happen at the
+  exact wrong times.
+
 v0.0.2 - 2021-02-19
 ~~~~~~~~~~~~~~~~~~~
 
--- carehare/_heartbeat.py
+++ carehare/_heartbeat.py
@@ -5,6 +5,18 @@ import pamqp.heartbeat
 
 _HEARTBEAT_BYTES = pamqp.heartbeat.Heartbeat.marshal()
 
+# The docs at https://www.rabbitmq.com/heartbeats.html are _really_
+# confusing, because they bring up the term "heartbeat interval" which
+# is used nowhere other than in the heartbeats.html file itself -- _and_
+# all the RabbitMQ discussion-forum questions and issue reports that
+# relate to the term in heartbeats.html.
+#
+# Here's the gist:
+# * The server will send 2 packets per `heartbeat` seconds.
+# * We, when receiving, will check every `heartbeat` seconds whether there
+#   has been traffic in the last `heartbeat` seconds. If not, we die.
+#   (This is what the RabbitMQ server does.)
+
 
 class HeartbeatSender:
     """Periodically send heartbeat frames over `transport`.
@@ -64,24 +76,23 @@ class HeartbeatMonitor:
         self._on_dead = on_dead
         self._loop = asyncio.get_running_loop()
         self._t0 = self._loop.time()
+        self._saw_data_since_last_beat = True
         self._n = 1
-        self._n_silent_intervals = 0
-        self._timer = self._loop.call_at(self._t0 + heartbeat / 2, self._beat)
+        self._timer = self._loop.call_at(self._t0 + heartbeat, self._beat)
 
     def _beat(self):
         """Must be called by self._timer."""
-        self._n += 1
-        self._n_silent_intervals += 1
-
-        if self._n_silent_intervals >= 3:
-            self._on_dead()
-        else:
+        if self._saw_data_since_last_beat:
+            self._saw_data_since_last_beat = False
+            self._n += 1
             self._timer = self._loop.call_at(
-                self._t0 + self._heartbeat * self._n / 2, self._beat
+                self._t0 + self._heartbeat * self._n, self._beat
             )
+        else:
+            self._on_dead()
 
     def reset(self):
-        self._n_silent_intervals = 0
+        self._saw_data_since_last_beat = True
 
     def cancel(self):
         self._timer.cancel()
